8ef2ff2b2720ef64125e41288f70bedfba6732db
Ahmet Uyar <ahmetuyar@gmail.com>
2021-06-24 13:15:24 +0300

    modified cluster-startup.sh for better readability and added some parameters

--- gpu_bdb/cluster_configuration/cluster-startup.sh
+++ gpu_bdb/cluster_configuration/cluster-startup.sh
@@ -8,16 +8,24 @@ USERNAME=$(whoami)
 # Scheduler configuration for your machine or cluster
 SCHEDULER_PORT=${SCHEDULER_PORT:-8786}
 DASHBOARD_ADDRESS=${DASHBOARD_ADDRESS:-8787}
+############################## MAYBE MODIFY ##################
 INTERFACE=${INTERFACE:-ib0}
+# INTERFACE=${INTERFACE:-enp129s0f1}
 
 # Logging and scratch space for your machine or cluster
-LOCAL_DIRECTORY=${LOCAL_DIRECTORY:-/raid/$USERNAME/dask-local-directory}
+# Repository home
+##############################  MODIFY ##################
+GPU_BDB_HOME=${GPU_BDB_HOME:-/path-to-project-dir/gpu-bdb}
+
+LOCAL_DIRECTORY=${LOCAL_DIRECTORY:-$GPU_BDB_HOME/dask-local-directory}
 SCHEDULER_FILE=${SCHEDULER_FILE:-$LOCAL_DIRECTORY/scheduler.json}
 LOGDIR=${LOGDIR:-$LOCAL_DIRECTORY/logs}
-WORKER_DIR=${WORKER_DIR:-/raid/$USERNAME/gpu-bdb-dask-workers/}
+WORKER_DIR=${WORKER_DIR:-$GPU_BDB_HOME/gpu-bdb-dask-workers/}
 
 # Communication protocol
-CLUSTER_MODE=${CLUSTER_MODE:-TCP}
+############################## MAYBE MODIFY ##################
+# CLUSTER_MODE=${CLUSTER_MODE:-TCP}
+CLUSTER_MODE=${CLUSTER_MODE:-NVLINK}
 
 # Cluster memory configuration
 MAX_SYSTEM_MEMORY=$(free -m | awk '/^Mem:/{print $2}')M
@@ -25,11 +33,8 @@ DEVICE_MEMORY_LIMIT=${DEVICE_MEMORY_LIMIT:-15GB}
 POOL_SIZE=${POOL_SIZE:-29GB}
 
 # Conda environment information
-CONDA_ENV_NAME=${CONDA_ENV_NAME:-rapids-gpu-bdb}
-CONDA_ENV_PATH=${CONDA_ENV_PATH:-/raid/$USERNAME/miniconda3/etc/profile.d/conda.sh}
-
-# Repository home
-GPU_BDB_HOME=${GPU_BDB_HOME:-/raid/$USERNAME/prod/gpu-bdb}
+CONDA_ENV_NAME=${CONDA_ENV_NAME:-gpu_bdb_dev}
+# CONDA_ENV_PATH=${CONDA_ENV_PATH:-/raid/$USERNAME/miniconda3/etc/profile.d/conda.sh}
 
 # Dask-cuda optional configuration
 export DASK_JIT_UNSPILL=${DASK_JIT_UNSPILL:-True}
@@ -62,32 +67,63 @@ rm -rf ~/.config/dask
 ### Launch the cluster ### #########################################################
 
 # Activate conda environment and install the local library
-source $CONDA_ENV_PATH
-conda activate $CONDA_ENV_NAME
+# source $CONDA_ENV_PATH
+# conda activate $CONDA_ENV_NAME
 
-cd $GPU_BDB_HOME/gpu_bdb
-python -m pip install .
+# cd $GPU_BDB_HOME/gpu_bdb
+# python -m pip install .
 
 
 if [ "$ROLE" = "SCHEDULER" ]; then
   if [ "$CLUSTER_MODE" = "NVLINK" ]; then
-     echo "Starting UCX scheduler.."
-     CUDA_VISIBLE_DEVICES='0' DASK_UCX__CUDA_COPY=True DASK_UCX__TCP=True DASK_UCX__NVLINK=True DASK_UCX__INFINIBAND=False DASK_UCX__RDMACM=False nohup dask-scheduler --dashboard-address $DASHBOARD_ADDRESS --port $SCHEDULER_PORT --interface $INTERFACE --protocol ucx --scheduler-file $SCHEDULER_FILE > $LOGDIR/scheduler.log 2>&1 &
+     echo "Starting UCX scheduler ..."
+     CUDA_VISIBLE_DEVICES='0' \
+     DASK_UCX__CUDA_COPY=True \
+     DASK_UCX__TCP=True \
+     DASK_UCX__NVLINK=True \
+     DASK_UCX__INFINIBAND=False \
+     DASK_UCX__RDMACM=False \
+     nohup dask-scheduler --dashboard-address $DASHBOARD_ADDRESS \
+                          --port $SCHEDULER_PORT \
+                          --interface $INTERFACE \
+                          --protocol ucx \
+                          --scheduler-file $SCHEDULER_FILE \
+                          > $LOGDIR/scheduler.log 2>&1 &
   fi
   
   if [ "$CLUSTER_MODE" = "TCP" ]; then
-     echo "Starting TCP scheduler.."
-     CUDA_VISIBLE_DEVICES='0' nohup dask-scheduler --dashboard-address $DASHBOARD_ADDRESS --port $SCHEDULER_PORT --interface $INTERFACE --protocol tcp --scheduler-file $SCHEDULER_FILE > $LOGDIR/scheduler.log 2>&1 &
+     echo "Starting TCP scheduler ..."
+     CUDA_VISIBLE_DEVICES='0' \
+     nohup dask-scheduler --dashboard-address $DASHBOARD_ADDRESS \
+                          --port $SCHEDULER_PORT \
+                          --interface $INTERFACE \
+                          --protocol tcp \
+                          --scheduler-file $SCHEDULER_FILE \
+                          > $LOGDIR/scheduler.log 2>&1 &
   fi
 fi
 
 # Setup workers
 if [ "$CLUSTER_MODE" = "NVLINK" ]; then
-    echo "Starting workers.."
-    dask-cuda-worker --device-memory-limit $DEVICE_MEMORY_LIMIT --local-directory $WORKER_DIR  --rmm-pool-size $POOL_SIZE --memory-limit $MAX_SYSTEM_MEMORY --enable-tcp-over-ucx --enable-nvlink  --disable-infiniband --scheduler-file $SCHEDULER_FILE >> $LOGDIR/worker.log 2>&1 &
+    echo "Starting workers with NVLINK ..."
+#    CUDA_VISIBLE_DEVICES=0 \
+    dask-cuda-worker --device-memory-limit $DEVICE_MEMORY_LIMIT \
+                     --local-directory $WORKER_DIR \
+                     --rmm-pool-size $POOL_SIZE \
+                     --memory-limit $MAX_SYSTEM_MEMORY \
+                     --enable-tcp-over-ucx \
+                     --enable-nvlink  \
+                     --disable-infiniband \
+                     --scheduler-file $SCHEDULER_FILE \
+                     >> $LOGDIR/worker.log 2>&1 &
 fi
 
 if [ "$CLUSTER_MODE" = "TCP" ]; then
-    echo "Starting workers.."
-    dask-cuda-worker --device-memory-limit $DEVICE_MEMORY_LIMIT --local-directory $WORKER_DIR  --rmm-pool-size $POOL_SIZE --memory-limit=$MAX_SYSTEM_MEMORY --scheduler-file $SCHEDULER_FILE >> $LOGDIR/worker.log 2>&1 &
+    echo "Starting workers with TCP ..."
+    dask-cuda-worker --device-memory-limit $DEVICE_MEMORY_LIMIT \
+                     --local-directory $WORKER_DIR  \
+                     --rmm-pool-size $POOL_SIZE \
+                     --memory-limit=$MAX_SYSTEM_MEMORY \
+                     --scheduler-file $SCHEDULER_FILE \
+                     >> $LOGDIR/worker.log 2>&1 &
 fi
